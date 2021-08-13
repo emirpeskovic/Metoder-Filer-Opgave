b54c50829460490d2259394b98bb0e929bed8ff7
Emir <me@emir.dk>
2021-08-13 10:10:49 +0200

    Opgave 4-6

--- Program.cs
+++ Program.cs
@@ -12,6 +12,15 @@ namespace Metoder_Filer_Opgave
             var content = File.ReadAllText(@".\StarWars.txt");
             Console.WriteLine(content);
             File.Delete(@".\StarWars.txt");
+
+            // Opgave 4-6
+            Directory.CreateDirectory(@".\Droids");
+            Directory.Delete(@".\Droids", true);
+            Directory.CreateDirectory(@".\Droids\StarWarsSpoilers");
+            File.WriteAllText(@".\Droids\StarWarsSpoilers\JegHarAldrigSetStarWars.txt", "Men jeg tror at Han ikke skød først");
+            var files = Directory.GetFiles(@".\Droids", "*.txt", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
         }
     }
 }
