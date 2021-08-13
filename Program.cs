b850f1c2b50f969b3da830f30d39f36fc4086619
Emir <me@emir.dk>
2021-08-13 10:02:49 +0200

    Opgave 1-3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Metoder_Filer_Opgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave 1-3
+            File.WriteAllText(@".\StarWars.txt", "Han shot first");
+            var content = File.ReadAllText(@".\StarWars.txt");
+            Console.WriteLine(content);
+            File.Delete(@".\StarWars.txt");
+        }
+    }
+}
